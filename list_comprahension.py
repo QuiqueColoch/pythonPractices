ace86265fb042bad863c9aa2a8eb5decae6f4226
Enrique Coloch <quiquecoloch@gmail.com>
2021-08-18 22:11:22 -0600

    List comprenhentions and high functions

--- list_comprahension.py
+++ list_comprahension.py
@@ -5,10 +5,15 @@ def run():
     #        squares.append(i**2)
     squares = [i**2 for i in range(1,101) if i%3 !=0]  
     list_challenge = [ i for i in range(1, 100000) if i % 36 == 0]
-
     #print(squares)
     for i in range(0,10):
         print(list_challenge[i])
+
+    my_list = [1,2,3,4,5]
+    list_challenge2 = [i**2 for i in my_list]
+    print ("Lista 2: " + str(list_challenge2))
+
+    
 
 if __name__ == '__main__':
     run()
